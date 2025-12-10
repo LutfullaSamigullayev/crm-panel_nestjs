f98dccb84029c62afb72e9b49ec5152309f52aeb
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-10 15:29:11 +0500

    import app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    StudentModule,
 
   ],
   controllers: [],
