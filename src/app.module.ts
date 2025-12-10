4ccb54691129c09cca0ce7db7b86bd2523456475
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-12-10 23:20:04 +0500

    import config and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,32 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StudentModule } from './student/student.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { StudentModule } from "./student/student.module";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { join } from "path";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+    }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
+      type: "postgres",
+      host: "localhost",
       port: 5432,
-      username: 'postgres',
+      username: "postgres",
       password: String(process.env.DB_PASSWORD as string),
       database: String(process.env.DB_DATABASE as string),
       autoLoadEntities: true,
       synchronize: true,
     }),
     StudentModule,
-
   ],
   controllers: [],
   providers: [],
